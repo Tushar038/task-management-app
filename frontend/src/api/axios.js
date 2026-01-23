ec8c513a031c0765afd73868091b7f6991bc6018
Tushar Nagar <tusharnagar83@gmail.com>
2026-01-23 20:43:12 +0000

    updated url for backend which is live on render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://task-management-app-production-f119.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
